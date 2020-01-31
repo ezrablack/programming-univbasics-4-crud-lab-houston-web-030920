428c33037bfa2e1df534882b030c2ba3308f8972
Timothy Morales <timothydanielmorales@gmail.com>
2020-01-31 05:16:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  add_element_to_end_of_array.pop
+  hobbies = ["food", "music", "camping", "cooking"]
+  hobbies.pop
 end
 
 def remove_element_from_start_of_array(array)
