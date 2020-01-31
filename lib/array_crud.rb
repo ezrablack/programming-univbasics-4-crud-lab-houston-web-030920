de312c569f70ee84e0d32a11f92e7a55f013b309
Timothy Morales <timothydanielmorales@gmail.com>
2020-01-31 05:24:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning"]
-  array.index[]
+  array[]
 end
 
 def retrieve_first_element_from_array(array)
