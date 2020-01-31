418cc39b033ebff5caab7587745a31580d39b1a1
Timothy Morales <timothydanielmorales@gmail.com>
2020-01-31 05:10:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   hobbies = ["food", "music", "camping", "cooking"]
-  hobbies << "Cooper"
+  hobbies << ""
 end
 
 def add_element_to_start_of_array(array, element)
